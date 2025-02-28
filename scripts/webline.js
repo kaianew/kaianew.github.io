7f29875cb438be790158c9be1bcc95b99a349b4f
Kaia Newman <klnewman@andrew.cmu.edu>
2025-02-28 16:18:07 -0500

    Created the twitching whiskers probability distribution function.

---
+++ scripts/webline.js
@@ -0,0 +1,11 @@
+var links = [
+    "https://schare.space",
+    "https://hgouni.com",
+    "https://veronicapim.github.io/"
+];
+
+function weblineWarp() {
+    var randIdx = Math.floor(Math.random() * links.length);
+    var link = links[randIdx];
+    window.open(link, "_self");
+}
